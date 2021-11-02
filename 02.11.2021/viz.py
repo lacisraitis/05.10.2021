d1ba5fee42d8576b4487b1f2099c1c5a1a25a8f0
lacisraitis <91954392+lacisraitis@users.noreply.github.com>
2021-11-02 12:51:08 +0000

    pievienoju failu

---
+++ 02.11.2021/viz.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+label = tk.Label()
+text = "sveiki! Sveiki! Sveiki!
+foreground = "red",
+background = #34A2FE"
+)
+
+label1 = tk.Label(
+  text = "Čau!",
+fg = "red",
+bg = "#34A2FE"
+width = 11,
+height = 10
+)
+
+label.pack()
+label1.pack()
+
+window.mainloop()
